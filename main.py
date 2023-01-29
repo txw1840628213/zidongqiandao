9e19abd2f0adc46582afdc6cd15c42eb62bcf583
txw1840628213 <106915865+txw1840628213@users.noreply.github.com>
2023-01-29 10:08:11 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def ooequ(i):
     requests.packages.urllib3.disable_warnings()
     res = requests.post(url=url,headers=headers,verify=False)
     #获取时间
-    t=datetime.now()
+    t=datetime.now(pytz.timezone('PRC'))
     #声明全局变量
     global s1
     s1 = t.strftime("%Y-%m-%d %H:%M:%S") 
