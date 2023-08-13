6c7c309a8f7cecdc50659ec7e2866fd7a4316a1b
txw1840628213 <106915865+txw1840628213@users.noreply.github.com>
2023-08-14 00:17:35 +0800

    Create sendemail.py

---
+++ git/sendemail.py
@@ -0,0 +1,33 @@
+import subprocess
+import smtplib
+from email.mime.text import MIMEText
+import datetime
+import time
+import os
+def send_email(i,ips,subject):
+    Email = os.getenv('EMAILS').split('\r\n')
+    # Change to your own account information
+    # Account Information
+    to            = Email[i]          # Email to send to.
+    mail_user     = os.getenv('MAIL_USER')           # Email to send from.
+    mail_password = os.getenv('MAIL_PASSWORD')          # Email password.
+    smtpserver    = smtplib.SMTP('smtp.qq.com') # Server to use.
+
+    smtpserver.ehlo()                            # Says 'hello' to the server
+    smtpserver.starttls()                        # Start TLS encryption
+    smtpserver.ehlo()
+    smtpserver.login(mail_user, mail_password)   # Log in to server
+    today = datetime.date.today()                # Get current time/date
+    
+
+    # Creates the text, subject, 'from', and 'to' of the message.
+    msg = MIMEText(ips)
+    msg['Subject'] = subject+'—%s' % today.strftime('%b %d %Y')
+    msg['From'] = mail_user
+    msg['To'] = to
+    print('邮件发送'+str(i))
+    # Sends the message
+    smtpserver.sendmail(mail_user, [to], msg.as_string())
+
+    # Closes the smtp server.
+    smtpserver.quit()
