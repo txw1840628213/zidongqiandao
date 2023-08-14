ad4bdee5d0d5835df3e855716ae966cfe89e8b6b
txw1840628213 <106915865+txw1840628213@users.noreply.github.com>
2023-08-14 09:46:12 +0800

    Update main.py

--- git/main.py
+++ git/main.py
@@ -17,18 +17,33 @@ def read():
 #根据列表网页请求，错误进行保留
 def re(name,t=0):
     shibai=[]
+    headers = {
+    'authority': 'epg.112114.eu.org',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+    'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+    'cache-control': 'max-age=0',
+    'sec-ch-ua': '"Not/A)Brand";v="99", "Microsoft Edge";v="115", "Chromium";v="115"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'none',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203',
+}
     for i in name:
-        for c in range(15):
+        for c in range(10):
             print(i+'第',c,'次')
             url = "http://epg.112114.eu.org/?ch="+i+"&date="+data(t)
             
             requests.packages.urllib3.disable_warnings()
             # 闪出警告
         
-            res = requests.get(url=url, verify=False)
+            res = requests.get(url=url,headers=headers verify=False)
             sleep(1)
             print(res.status_code)
-            if res.status_code == 200:
+            if res.status_code == 200 || res.status_code == 201:
                 file(i,res.text,t)
                 break
             elif c == 9:
